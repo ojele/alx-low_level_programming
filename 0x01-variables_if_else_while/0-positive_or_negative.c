b7017d410714caaf62e42243a45485fdacc66ff8
ojele <ojelesamuel2@gmail.com>
2022-07-01 21:42:52 -0700

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,20 +5,20 @@
 /**
  *main -> assign a random number to the variable n each time it is executed and prints out
  *based a condition
- *Return : always zero
+ *Return : always 0
  */
  int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
 	printf("%d is positive\n", n);
-if (n == 0)
+	if (n == 0)
 	printf("%d is zero\n",n);
-if (n < 0){
+	if (n < 0){
 	printf("%d is negative\n", n);
-}
-return (0);
+	}
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,31 +3,33 @@
 #include <stdio.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed and prints out
- *based a condition
- *Return : always zero
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
  int main(void)
 {
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d ls ",n);
-if (n > 5)
-{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d ls ",n);
+	if (n > 5)
+	{
 	printf("greater than 5");
-}
-if (n == 0)
-{
+	}
+	if (n == 0)
+	{
 	printf("0");
-}
-if (n < 6 && n !=0)
-{
+	}
+	if (n < 6 && n !=0)
+	{
 	printf("less than 6 not 0");
-}
+	}
 
-printf("\n");
+	printf("\n");
 
-return (0);
+	return (0);
 }
