66fcef9ae144199f1d51d4c64552cf16458b601c
ojele <ojelesamuel2@gmail.com>
2022-07-03 09:57:43 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - entry point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z' ; a++)
+{
+if (a == 'q' || a == 'e')
+{
+continue;
+}
+putchar(a);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - entry point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+char a;
+int a - 0;
+for (a = '7'; a >= 'a'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
