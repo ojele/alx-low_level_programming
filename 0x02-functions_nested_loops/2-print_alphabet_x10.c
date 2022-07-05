cacebe175d1a74f910189a3df7b1f13eb9dd923f
ojele <ojelesamuel2@gmail.com>
2022-07-05 09:37:00 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints the lower alphabets
+*/
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; X < 10; x++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n')
+	}
+}
