06f46437cd57cbfc282d03630feb7af32f87ca49
ojele <ojelesamuel2@gmail.com>
2022-07-06 08:01:30 -0700

    ask

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,15 +6,15 @@
 */
 void print_times_table(int n)
 {
-	int i, j, x
-	
+	int i, j, x;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				x =j * i;
+				x = j * i;
 				if (j == 0)
 				{
 					_putchar(x + '0');
