1625000d90409a9bf5878aa20b619de241619330
ojele <ojelesamuel2@gmail.com>
2022-07-05 22:09:14 -0700

    an

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d\n");
 			else
-				printf("%d,",n);
+				printf("%d,", n);
 			n = n + 1;
 		}
 	}
@@ -24,9 +24,9 @@ void print_to_98(int n)
 		while (n >= 98)
 		{
 			if (n == 98)
-				printf("%d\n",n);
+				printf("%d\n", n);
 			else
-				printf("%d,",n);
+				printf("%d,", n);
 			n = n - 1;
 		}
 	}
