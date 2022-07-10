aed134812ef542895d1d721ce03e60976bc85557
ojele <ojelesamuel2@gmail.com>
2022-07-10 13:02:09 -0700

    update

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,17 @@ if (a > b && a > c)
 {
 	largest = a;
 }
-else if (b > a && b > c)
+else if (b > c && b > a)
 {
 	largest = b;
 }
-else
+else if (c > b)
 {
 	largest = c;
 }
-
-	return (largest);
+else
+{
+	largest = b;
+}
+return (largest);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 
 /**
 * print_remaining_days - takes a date and prints how many days are 
-* left in the year,taking the leap years into accounr
+* left in the year,taking the leap years into account
 * @month: month in number format
 * @day: day of month
 * @year: year
@@ -10,4 +10,26 @@
 */
 
 void print_remaining_days(int month, int day, int year)
+	if ((year % 100 == 0 && year % 400b== 0) || (year % 4 == 0))
+	{
+		if (month > 2 && day >= 60)
+	 	{
+	 		day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remainig days: %d\n", 365 = day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 = day);
+		}
+	}
+}
 
