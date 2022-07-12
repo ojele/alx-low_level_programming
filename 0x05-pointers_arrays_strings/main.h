898e19932f9db0b05f10c2d1d096e471bf22125c
ojele <ojelesamuel2@gmail.com>
2022-07-12 01:45:23 -0700

    real

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,5 +10,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
