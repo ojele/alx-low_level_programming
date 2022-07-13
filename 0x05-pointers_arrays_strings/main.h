cc4c16be346e03604578b0feff360fbd7b12211f
ojele <ojelesamuel2@gmail.com>
2022-07-12 21:41:48 -0700

    ask

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 void reset_to_98(int *n);
 	void swap_int(int *a, int *b);
 	int _strlen(char *s);
@@ -9,5 +11,4 @@ void reset_to_98(int *n);
 	void print_array(int *a, int n);
 	char *_strcpy(char *dest, char *src);
 	void rev_string(char *s);
-
-
+#endif
