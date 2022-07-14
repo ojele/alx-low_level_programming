91fa7e2dfe751b999155b91269b2d0419dfd30df
ojele <ojelesamuel2@gmail.com>
2022-07-14 09:17:47 -0700

    leet is done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* leet - > a leet function
+* @X: param X
+* Return: a string
+*/
+char *leet(char *X)
+{
+	int a = 0; b = 0; l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char trw[5] = {'4', '3', '0', '7', '1'};
+
+	while (X[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (X[a] == tr[b] || X[a] - 32 == tr[b])
+				X[a] = trw[b];
+			b++;
+		}
+		a++;
+	}
+	return (X);
+}
+
