6eebe2b3c126aa4ed71f29075d3ea3f32490c1e3
ojele <ojelesamuel2@gmail.com>
2022-07-14 04:25:25 -0700

    ask

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,9 +14,9 @@ char *_strcat(char *dest, char *src)
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
-		dest[len] = src[i]
+		dest[len] = src[i];
 		len += 1;
 	}
 	dest[len] = '\0';
