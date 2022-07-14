e491813ae509e14bb790fa826c15621265f3b269
ojele <ojelesamuel2@gmail.com>
2022-07-14 04:38:01 -0700

    new

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncpy -> for copying purpose
+* @dest: param1
+* @src: param2
+* @n: param2
+* Retrun: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
